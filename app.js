fe10ba6b746df65c3208c7ba0d1ead079bd27209
cotsao <codytsao@gmail.com>
2021-07-07 20:57:22 -0700

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ for (let i = 0; i <= 200; i+=2)
 {
     console.log(i)
 }
+// 3
+for (let i =0; i<20; i++)
+{
+    let arr = 
+        ["...human...why you taking pictures of me?...",
+         "...the catnip made me do it...",  
+         "...why does the red dot always get away..."]
+    console.log('Love me, pet me! HSSSSSS!')
+    if (i%2 ===0)
+    {
+        console.log(arr[Math.floor(Math.random() * arr.length) ] )
+    }
+}
