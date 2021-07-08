4f1de5a1aa21cffd771f4ab70df5446d23c9de59
nndeto <n_ndeto@yahoo.com>
2021-07-08 19:57:18 -0400

    easy going answered

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-//Question 1
+//Easy Going Question 1
+for (let i= 0; i<=20; i++) {
+    console.log(i);
+}
+
+
+//Get Even Question 1
 for (let i = 0; i <= 200; i+=2) {
     console.log(i);
+}
+
+//Question 2
+for (let i = 0; i < 20; i++) {
+
 }
