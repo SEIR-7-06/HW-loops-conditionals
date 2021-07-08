34e4b243fc249d21e59381ab51a87057cf134b0d
cotsao <codytsao@gmail.com>
2021-07-07 21:16:24 -0700

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -22,3 +22,33 @@ for (let i =0; i<20; i++)
         console.log(arr[Math.floor(Math.random() * arr.length) ] )
     }
 }
+// 4
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+/* 1 */
+kenny[0] = "Gameboy"
+/* 2 */
+jimClark[1]+=1 
+/* 3 */
+ryan[2] = "Gotham City"
+/* 4 */
+/* Instructions here say
+"Reuben left Durham 5 years ago to come to Chicago.
+First, remove "Durham" from Reuben's array, and then add "Chicago".
+(Note: remove and then add is different from simply changing the value at that index.)" 
+Nothing else to do?*/
+reuben.pop()
+reuben.push('Chicago')
+/* 5 */
+jimHaff.pop()
+jimHaff.push('Taipei', 'Taichung', 'Paris')
+/* bonus */
+jimHaff.splice(2,1,'')
+
+
+
+
+
