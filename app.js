fd1aac97636b23e03f236607280768d4fd7c5b5c
DataPlant <dataplant@outlook.com>
2021-07-08 23:02:06 -0400

    Commit 3 again bc missed a change due to it not being added before commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ for(let i=0; i < 20; i++){
 let reaction = ["...human...why you taking pictures of me?...","...the catnip made me do it...","...why does the red dot always get away..."]
 for(let i=0; i < 20; i++){
     if(i%2 === 0){
-        rando= Math.floor(Math.random()*2)
+        rando= Math.floor(Math.random()*3)
         console.log(reaction[rando])
     }
     console.log("Love me, pet me! HSSSSSS!")
