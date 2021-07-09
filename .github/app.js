a06ab16e53d6dc235daabb7138f710ffe5230283
stephanyburden <stephanyburden@gmail.com>
2021-07-08 23:12:49 -0700

    Getting to Know You answered

--- .github/app.js
+++ .github/app.js
@@ -27,3 +27,27 @@ const random = Math.floor(Math.random() * catTalk.length);
 for(let i = 0; i < 20; i+=2) {
     console.log(catTalk);
 }
+
+//  Working with Arrays
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+
+kenny.splice(0, 1, "Gameboy");
+console.log(kenny);
+
+jimClark.splice(1, 1, 187);
+console.log(jimClark);
+
+ryan.splice(2, 1, "Gatham City");
+console.log(ryan);
+
+reuben.splice(2, 1, "Chicago");
+console.log(reuben);
+
+jimHaff.splice(2, 1, "ATL", "Brooklyn", "D.C")
+console.log(jimHaff);
