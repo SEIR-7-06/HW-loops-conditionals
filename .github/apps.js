9531137b0b34f092cdf92bb42459a8bd6df9f69d
gonzzoh <gmeza0118@gmail.com>
2021-07-08 22:23:47 -0400

    finishes Get Even

---
+++ .github/apps.js
@@ -0,0 +1,15 @@
+// Get Even 
+
+// for (i = 0; i <= 200; i++){
+//     console.log(i)
+// }
+
+//Excited Kitten
+
+let hiveMind = ["...human...why you taking pictures of me?...",
+ "...the catnip made me do it...", 
+"...why does the red dot always get away..."]
+
+for (affection = 0; affection <= 20; affection++){
+    console.log("Love me, pet me! HSSSSSS!")
+}
