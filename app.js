85f9a500bf0ab15344524445721e4fb2a29c5eda
DataPlant <dataplant@outlook.com>
2021-07-09 01:12:49 -0400

    07/09/2021 Save

--- app.js
+++ app.js
@@ -18,3 +18,27 @@ for(let i=0; i < 20; i++){
     }
     console.log("Love me, pet me! HSSSSSS!")
 }
+//Working Arrays
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny.splice(0, 1, "Gameboy");
+console.log(kenny[0])
+
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark[1])
+
+for(let i = 0; i < ryan.length; i++){
+    if(ryan[i] === "Denver"){
+        ryan[i] = "Gotham City"
+        x = i
+    }
+}
+console.log(ryan[x])
+
+reuben.splice(2, 1, "Chicago")
+console.log(reuben[2])
+
