0924130e9ee6d5ad1cf6c199ee34f8f7c363806c
jekkiboi <hollymayer4@gmail.com>
2021-07-08 18:20:19 -0700

    Commit 5 - Getting to Know You answered

--- HW-Loops.js
+++ HW-Loops.js
@@ -1,9 +1,45 @@
+///////QUESTION 1////////////
 // for(i = 1; i <=20; i++){
 //     console.log(i)
 // }
+///////QUESTION 2////////////
 // for(i = 0; i <=200; i+=2){
 //     console.log(i)
 // }
-for(i = 1; i <=20; i++){
-    console.log("Love me, pet me! HSSSSSS!")
-}
+///////QUESTION 3////////////
+// for(i = 1; i <=20; i++){
+//     console.log("Love me, pet me! HSSSSSS!")
+// }
+///////QUESTION 4.1////////////
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny.splice(0,1,"Gameboy");
+console.log(kenny);
+
+//QUESTION 4.2
+
+jimClark[1] = jimClark[1]+1;
+console.log(jimClark);
+
+//QUESTION 4.3
+ryan.splice(2, 1, "Gotham City")
+console.log(ryan);
+
+//QUESTION 4.4
+reuben.pop("Durham");
+console.log(reuben);
+
+reuben.push("Chicago")
+console.log(reuben);
+
+//QUESTION 4.5
+jimHaff.splice(2, 1, "Houston" , "Springfield" , "Boise")
+console.log(jimHaff);
+
+//QUESTION 4.6 BONUS
+jimHaff.splice(2, 1, 0)
+console.log(jimHaff);
