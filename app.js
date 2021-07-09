59747c133cda4a2163d7236c931a794783ee300c
MisterManley <george@themanleystudio.com>
2021-07-08 21:21:33 -0500

    Commit 7 - Kristyn and Thom have their outfits ready for class - array practice

--- app.js
+++ app.js
@@ -34,9 +34,44 @@
 // jimHaff.splice(2, 1);
 // console.log(kenny, jimClark, reuben, jimHaff, ryan);
 
-let ninjaTurtles = ["Donatello", "Leonardo", "Michaelangelo", "Rafael"];
+// let ninjaTurtles = ["Donatello", "Leonardo", "Michaelangelo", "Rafael"];
 
-for(let i = 0; i < ninjaTurtles.length; i++){
-    let yell = ninjaTurtles[i].toUpperCase();
-    console.log(yell + "!");
-}
+// for(let i = 0; i < ninjaTurtles.length; i++){
+//     let yell = ninjaTurtles[i].toUpperCase();
+//     console.log(yell + "!");
+// }
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+  krystnsShoe = kristynsCloset.splice(0,1).toString();
+thomsCloset[2].push(krystnsShoe);
+console.log(thomsCloset[2]);
