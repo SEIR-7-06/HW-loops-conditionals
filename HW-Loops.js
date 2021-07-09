1c38028f965a5465b4bb254bb90ff8cada5394af
jekkiboi <hollymayer4@gmail.com>
2021-07-08 17:41:45 -0700

    Commit 3 - Excited Kittens answered

--- HW-Loops.js
+++ HW-Loops.js
@@ -1,6 +1,9 @@
 // for(i = 1; i <=20; i++){
 //     console.log(i)
 // }
-for(i = 0; i <=200; i+=2){
-    console.log(i)
+// for(i = 0; i <=200; i+=2){
+//     console.log(i)
+// }
+for(i = 1; i <=20; i++){
+    console.log("Love me, pet me! HSSSSSS!")
 }
