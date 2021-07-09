46b30c13bc53e1b56f2eb45d162a6bb0d820fb60
MisterManley <george@themanleystudio.com>
2021-07-08 22:16:09 -0500

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -41,69 +41,86 @@
 //     console.log(yell + "!");
 // }
 
-const kristynsCloset = [
-    "left shoe",
-    "cowboy boots",
-    "right sock",
-    "GA hoodie",
-    "green pants",
-    "yellow knit hat",
-    "marshmallow peeps"
-  ];
+// const kristynsCloset = [
+//     "left shoe",
+//     "cowboy boots",
+//     "right sock",
+//     "GA hoodie",
+//     "green pants",
+//     "yellow knit hat",
+//     "marshmallow peeps"
+//   ];
   
-  // Thom's closet is more complicated. Check out this nested data structure!!
-  const thomsCloset = [
-    [
-      // These are Thom's shirts
-      "grey button-up",
-      "dark grey button-up",
-      "light blue button-up",
-      "blue button-up",
-    ],[
-      // These are Thom's pants
-      "grey jeans",
-      "jeans",
-      "PJs"
-    ],[
-      // Thom's accessories
-      "wool mittens",
-      "wool scarf",
-      "raybans"
-    ]
-  ];
+//   // Thom's closet is more complicated. Check out this nested data structure!!
+//   const thomsCloset = [
+//     [
+//       // These are Thom's shirts
+//       "grey button-up",
+//       "dark grey button-up",
+//       "light blue button-up",
+//       "blue button-up",
+//     ],[
+//       // These are Thom's pants
+//       "grey jeans",
+//       "jeans",
+//       "PJs"
+//     ],[
+//       // Thom's accessories
+//       "wool mittens",
+//       "wool scarf",
+//       "raybans"
+//     ]
+//   ];
 
-krystnsShoe = kristynsCloset.splice(0,1).toString();
-thomsCloset[2].push(krystnsShoe);
+// krystnsShoe = kristynsCloset.splice(0,1).toString();
+// thomsCloset[2].push(krystnsShoe);
 
-randomOutfit = [];
-randomOutfitK = kristynsCloset[Math.floor(Math.random() * (kristynsCloset.length -1))];
-randomOutfitT1 = thomsCloset[Math.floor([Math.random() * (thomsCloset.length - 1)])];
-randomOutfitT2 = randomOutfitT1[Math.floor([Math.random() * (randomOutfitT1.length - 1)])];
-console.log("Today, were going to be wearing the", randomOutfitK, "and", randomOutfitT2 + ".");
+// randomOutfit = [];
+// randomOutfitK = kristynsCloset[Math.floor(Math.random() * (kristynsCloset.length -1))];
+// randomOutfitT1 = thomsCloset[Math.floor([Math.random() * (thomsCloset.length - 1)])];
+// randomOutfitT2 = randomOutfitT1[Math.floor([Math.random() * (randomOutfitT1.length - 1)])];
+// console.log("Today, were going to be wearing the", randomOutfitK, "and", randomOutfitT2 + ".");
 
-for(let i = 0; i < kristynsCloset.length; i++){
-    console.log("WHIRR: Now washing (" + kristynsCloset[i] + ")");
-}
+// for(let i = 0; i < kristynsCloset.length; i++){
+//     console.log("WHIRR: Now washing (" + kristynsCloset[i] + ")");
+// }
 
 
 
-for(let j = 0; j < thomsCloset.length; j++){
+// for(let j = 0; j < thomsCloset.length; j++){
             
     
-            switch(j){
-            case 0:
-                console.log("Thom's shirts");
-                break;
-            case 1:
-                console.log("Thom's pants");
-                break;
-            case 2:
-                console.log("Thom's accessories");
-                break;
-            default:
-                console.log("Hey! Where did all of Thom's stuff go???");
-                break;
+//             switch(j){
+//             case 0:
+//                 console.log("Thom's shirts");
+//                 break;
+//             case 1:
+//                 console.log("Thom's pants");
+//                 break;
+//             case 2:
+//                 console.log("Thom's accessories");
+//                 break;
+//             default:
+//                 console.log("Hey! Where did all of Thom's stuff go???");
+//                 break;
 
-        }
-}
+//         }
+// }
+
+//////////////////////  FIZZBUZZ //////////////////////////////
+
+for(let i = 1; i <= 100; i++){
+  
+    if(i % 3 == 0 && i % 5 == 0){
+      console.log("fizzbuzz " + i);
+    } else if(i % 3 == 0){
+        console.log("fizz " + i)
+      }else if(i % 5 == 0){
+        console.log("buzz " + i)
+      }else {
+        console.log(i);
+      }
+  }
+
+
 
