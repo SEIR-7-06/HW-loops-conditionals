6ad920dded8f19bd7187d7e3df0970f9b33209a6
nndeto <n_ndeto@yahoo.com>
2021-07-09 00:34:14 -0400

    excited kitten solved

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Easy Going Question 1
-for (let i= 0; i<=20; i++) {
+/*for (let i= 0; i<=20; i++) {
     console.log(i);
 }
 
@@ -9,7 +9,22 @@ for (let i = 0; i <= 200; i+=2) {
     console.log(i);
 }
 
-//Question 2
+//Excited Kitten Question 1
 for (let i = 0; i < 20; i++) {
+    console.log("Love me, pet me! Hssss!");
+}*/
 
+//Excited Kitten Question 2
+let catStatements = [
+    "human, why you taking pictures of me?", 
+    "the catnip made me do it", 
+    "why does the red dot always get away"];
+
+let catSays = catStatements[Math.floor(Math.random()*3)];
+
+let i;
+for (i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+        console.log(catSays);
+    }
 }
