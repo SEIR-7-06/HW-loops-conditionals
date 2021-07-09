b7acdf0b9cc07c543154b2d4c8aba991f2fa2f94
cotsao <codytsao@gmail.com>
2021-07-08 22:54:08 -0700

    Project Euler Problem 1 completed

--- app.js
+++ app.js
@@ -181,6 +181,16 @@ for (let i = 0; i <=100; i++)
     console.log(i)
   }
 }
+//Multiples of 3 and 5
+let sum = 0
+for (let i = 1; i < 1000; i++)
+{
+  if (i % 3 === 0 || i % 5 === 0)
+  {
+    sum+=i
+  }  
+}
+console.log(sum)
 
 
 
