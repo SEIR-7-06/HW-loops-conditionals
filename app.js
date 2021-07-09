755c61a1137551eeac715b1c192073fc5d884290
MisterManley <george@themanleystudio.com>
2021-07-08 21:12:17 -0500

    Commit 6 - Yell at the Ninja Turtles answered

--- app.js
+++ app.js
@@ -19,19 +19,24 @@
 //    excitedKitty = randomKitty[Math.floor(Math.random() * 3)];
 //}
 
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
 
-kenny[0] = "Gameboy";
-jimClark[1] = 187;
-ryan[2] = "Gotham City";
-reuben.splice(2, 1, "Chicago");
-jimHaff.pop();
-jimHaff.push("San Francisco", "Chicago", "Houston");
-jimHaff.splice(2, 1);
-console.log(kenny, jimClark, reuben, jimHaff, ryan);
+// kenny[0] = "Gameboy";
+// jimClark[1] = 187;
+// ryan[2] = "Gotham City";
+// reuben.splice(2, 1, "Chicago");
+// jimHaff.pop();
+// jimHaff.push("San Francisco", "Chicago", "Houston");
+// jimHaff.splice(2, 1);
+// console.log(kenny, jimClark, reuben, jimHaff, ryan);
 
-                                                                
+let ninjaTurtles = ["Donatello", "Leonardo", "Michaelangelo", "Rafael"];
+
+for(let i = 0; i < ninjaTurtles.length; i++){
+    let yell = ninjaTurtles[i].toUpperCase();
+    console.log(yell + "!");
+}
