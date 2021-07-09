9ee0a91f0076c85e33cb73049302ab86039f42e9
DataPlant <dataplant@outlook.com>
2021-07-09 16:54:33 -0400

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ const jimHaff = ["Jim H", 16, "All cities"];
 const reuben = ["Reuben", 22, "Durham"];
 const jimClark = ["Jim C", 186, "LA"];
 const ryan = ["Ryan", 65, "Denver"];
-
+//1.)
 kenny.splice(0, 1, "Gameboy");
 console.log(kenny[0])
-
+//2.)
 jimClark[1] = jimClark[1] + 1;
 console.log(jimClark[1])
-
+//3.)
 for(let i = 0; i < ryan.length; i++){
     if(ryan[i] === "Denver"){
         ryan[i] = "Gotham City"
@@ -38,10 +38,12 @@ for(let i = 0; i < ryan.length; i++){
     }
 }
 console.log(ryan[x])
-
+//4.)
 reuben.splice(2, 1, "Chicago")
 console.log(reuben[2])
-
+//5.)
 jimHaff.splice(2, 1, "Paris", "Rome", "Seattle")
 console.log(jimHaff)
-
+//6.) (This question needs to be commented out for #5 to show up correctly, otherwise it will also be missing Paris)
+jimHaff.splice(2,1)
+console.log(jimHaff)
