6e2864d648d57485c3d45c4944d5c651d7689f34
stephanyburden <stephanyburden@gmail.com>
2021-07-09 00:39:20 -0700

    Excited Kitten attempt2

--- .github/app.js
+++ .github/app.js
@@ -22,10 +22,10 @@ const catTalk = ['...human...why you taking pictures of me?...',
 '...the catnip made me do it...',
 '..why does the red dot always get away'
 ];
-const random = Math.floor(Math.random() * catTalk.length);
+const randomIndex = Math.floor(Math.random() * catTalk.length);
 
-for(let i = 0; i < 20; i+=2) {
-    console.log(catTalk);
+for(let i = 0; i < randomIndex; i+=2) {
+    console.log(catTalk[i]);
 }
 
 //  Working with Arrays
