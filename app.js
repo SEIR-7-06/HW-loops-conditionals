617df438174e95d4738687f565f36305dcf33290
DataPlant <dataplant@outlook.com>
2021-07-08 22:57:18 -0400

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ for(let i = 0; i < 201; i++){
         console.log(i)
     }
 }
+//Excited Kitten
+for(let i=0; i < 20; i++){
+    console.log("Love me, pet me! HSSSSSS!")
+}
+
+let reaction = ["...human...why you taking pictures of me?...","...the catnip made me do it...","...why does the red dot always get away..."]
+for(let i=0; i < 20; i++){
+    if(i%2 === 0){
+        rando= Math.floor(Math.random()*2)
+        console.log(reaction[rando])
+    }
+    console.log("Love me, pet me! HSSSSSS!")
+}
