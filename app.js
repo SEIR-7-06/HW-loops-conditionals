62e961be5adf6b1f804a8ee067b387352a06fd4e
cotsao <codytsao@gmail.com>
2021-07-08 22:28:19 -0700

    Kristyn and Thom have their oufits ready for class- array practice

--- app.js
+++ app.js
@@ -81,6 +81,79 @@ for (let i = 0; i< ninTurtles.length; i++)
     console.log(newString[i])
 }
 
+//6 
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+  let kristynsShoe = kristynsCloset.shift()
+  
+ 
+
+  for (let i = 0; i<3; i++)
+  {
+      const outfits = []
+      for (let k = 0; k <3; k++)
+      { 
+        let randNumber =  Math.floor( Math.random() * kristynsCloset.length)
+        outfits.push(kristynsCloset.splice(randNumber, 1) ) 
+      }
+      console.log("Outfit " + (i+1) + " for Kristyen is " + outfits)
+      while (outfits.length != 0)
+      {
+        kristynsCloset.push(outfits.pop())
+      }
+  } 
+
+  for (let i = 0; i <3; i++)
+  {
+    const thomsOutfit = []
+    for (let k = 0; k < 3; k++)
+    {
+      let randNumber = Math.floor (Math.random() * thomsCloset[k].length)
+      thomsOutfit.push( thomsCloset[k][randNumber])
+    }
+    console.log("Outfit " + (i+1) + " for Thom is " + thomsOutfit)
+    for (let k = 2; k!=0 ; k--)
+    {
+      thomsCloset[k].push(thomsOutfit.pop())
+    }
+  }
+
+     
+
+  
+
+/*   Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+
+  Modify your code to put together 3 separate outfits for Kristyn and Thom. Put the output in a sentence to tell us what we'll be wearing. Mix and match!
+ */
 
 
 
