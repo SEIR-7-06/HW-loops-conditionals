95281c2d5780a0e91bbd05c793339c714fdad05f
marshallwatrous <marshallwatrous95@gmail.com>
2021-07-10 16:43:33 -0700

    Commit 3 - Excited Kitten answered

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ for (let i = 1; i < 21; i++) {
 for (i = 0; i< 201; i+=2) {
     console.log(i);
 }
+
+// Excited Kitten
+
+for (let i = 1; i < 21; i++) {
+    console.log("Love me, pet me!  HSSSSSS!");
+    if (i % 2 == 0) {
+        let value = Math.floor(Math.random() * 3);
+        if (value == 0) {
+            console.log("...human...why you taking pictures of me?...");
+        } else if (value == 1) {
+            console.log("...the catnip made me do it...");
+        } else {
+            console.log("...why does the red dot always get away...");
+        }
+    }
+}
