ef41b0c18393db2b369ff080bec88a5b0e261654
brookecalhoun <brookecalhouncoding@gmail.com>
2021-07-11 10:15:44 -0700

    commit 3 - excited kittens answered

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ for(let i = 1; i <= 20; i++){
 for(let i = 0; i <= 200; i+=2){
     console.log(i)
 }
+
+//Excited Kitten
+
+for(let i = 1; i <= 20; i ++ ){
+    if(i % 2 === 0){
+        console.log("...human...why you taking pictures of me?...")
+    } else {
+    console.log("Love me, pet me! HSSSS!")
+    }
+}
