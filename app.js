09ed3c313dfc0b44ec2855054cacdc7b71525385
erininabox <erininabox@gmail.com>
2021-07-11 15:47:10 -0700

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -27,3 +27,44 @@ for (i=1; i<=20; i++) {
 
 /////////////////////////////////////////
 // Working with Arrays
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// Remove "Kenny" from the 'kenny' array and replace it with "Gameboy".
+
+kenny[0] = "Gameboy";
+console.log(kenny[0]);
+
+// Write code to add 1 year to Jim Clark's age.
+
+jimClark[1] = jimClark[1]+1;
+console.log(jimClark[1]);
+
+// Change Ryan's hometown from "Denver" to "Gotham City."
+
+ryan[2] = "Gotham City";
+console.log(ryan[2]);
+
+// First remove "Durham" from Reuben's array. Then, add "Chicago".
+
+reuben.pop();
+console.log(reuben);
+reuben.push("Chicago");
+console.log(reuben);
+
+// Jim Haff - remove "All cities" then add 3 cities to his array.
+
+jimHaff.pop();
+console.log(jimHaff);
+jimHaff.push("Tacoma", "San Francisco", "Boston");
+console.log(jimHaff);
+
+// Jimm Haff Bonus question - splice out the first of the 3 cities he lives in
+
+jimHaff.splice(2, 1);
+console.log(jimHaff);
+
