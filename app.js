24e061414ad6dc2c992c47a4988d3dab1168f4a6
marshallwatrous <marshallwatrous95@gmail.com>
2021-07-10 17:14:55 -0700

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -27,3 +27,43 @@ for (let i = 1; i < 21; i++) {
         }
     }
 }
+
+// Working with Arrays
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 1. 
+
+kenny[0] = "Gameboy";
+console.log(kenny);
+
+// 2.
+
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark);
+
+// 3.
+
+ryan[2] = "Gotham City";
+console.log(ryan);
+
+// 4. 
+
+reuben.pop();
+reuben.push("Chicago");
+console.log(reuben);
+
+// 5.
+
+jimHaff.pop();
+jimHaff.push("Seattle", "Tokyo", "Rome");
+console.log(jimHaff);
+
+//6. 
+
+jimHaff.splice(2, 1);
+console.log(jimHaff);
