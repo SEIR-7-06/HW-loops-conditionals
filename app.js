32359a19be5e741d3cfdce34b94b7eb263b9cfa4
erininabox <erininabox@gmail.com>
2021-07-11 14:56:42 -0700

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ for (i=1; i<=20; i++) {
 for (i=0; i<=200; i+=2) {
     console.log(i);
 }
+
+/////////////////////////////////////////
+// Excited Kitten
+
+for (i=1; i<=20; i++) {
+    console.log("Love me, pet me! HSSSSSS!");
+}
