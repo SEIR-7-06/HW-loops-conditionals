0e49b61bf1ef7b1c44152f32dc58ed6ea20e5ec4
off2mxrs <off2mxrs@gmail.com>
2021-07-12 00:34:31 -0700

    Getting to Know You

--- app.js
+++ app.js
@@ -24,3 +24,30 @@ for (i = 0; i <= 20; i++) {
         console.log("Love me, pet me! HSSSSSS!");
     }
 }
+
+
+// Working with arrays
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = 'Gameboy';
+console.log(kenny);
+
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark);
+
+ryan[2] = 'Gotham City';
+console.log(ryan);
+
+reuben.pop();
+reuben[2] = 'Chicago';
+console.log(reuben);
+
+jimHaff.pop();
+jimHaff.push('San Fran', 'Tokyo', 'Paris');
+jimHaff.splice(2, 1)
+console.log(jimHaff);
