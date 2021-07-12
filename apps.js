5ed0047774fb87166d993bf8bb006842b4e21e44
gonzzoh <gmeza0118@gmail.com>
2021-07-12 02:05:20 -0400

    Commit 5 - Getting to Know You answered

--- apps.js
+++ apps.js
@@ -13,19 +13,35 @@
 // Excited Kitten //
 
 
-let hiveMind = ['...human...why you taking pictures of me?...',
-'...the catnip made me do it...', 
-'...why does the red dot always get away...'];
-let meow = hiveMind[Math.floor(Math.random() * hiveMind.length)];
+// let hiveMind = ['...human...why you taking pictures of me?...',
+// '...the catnip made me do it...', 
+// '...why does the red dot always get away...'];
+// let meow = hiveMind[Math.floor(Math.random() * hiveMind.length)];
 
-// for (affection = 0; affection <= 20; affection++){
-    //     console.log("Love me, pet me! HSSSSSS!")
-    // }
     
-    for (let affection = 0; affection < 20; affection++){
-        if ( affection % 2 == 0){
-            console.log("Love me, pet me! HSSSSSS!");
-        } else { 
-            console.log(meow);
-        }
-}
+//     for (let affection = 0; affection < 20; affection++){
+//         if ( affection % 2 == 0){
+//             console.log("Love me, pet me! HSSSSSS!");
+//         } else { 
+//             console.log(meow);
+//         }
+// }
+
+// Working with arrays //
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny [0] = 'Gameboy';
+kenny [0] = 'Gameboy';
+jimClark.splice(1, 1, 187)
+ryan [2] = 'Gotham City';
+reuben.pop();
+reuben.push([2], 'Chicago')
+jimHaff.splice(2, 2, 'NY', 'Boston', 'Chicago');
+jimHaff.splice(2, 1 )
+
+console.log( kenny, jimHaff, reuben, jimClark, ryan);
