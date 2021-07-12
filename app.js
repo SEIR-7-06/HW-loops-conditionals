0e69f7737eb68ac4c9a30138149a3c849bb40772
off2mxrs <off2mxrs@gmail.com>
2021-07-12 00:10:36 -0700

    Excited Kittens answered

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ for (i = 1; i <= 20; i++){
 for (i = 0; i <= 200; i += 2) {
     console.log(i);
 }
+
+// Excited Kitten
+const cat = Math.random([
+    "...human...why you taking pictures of me?...", 
+    "...the catnip made me do it...",
+    "...why does the red dot always get away...",
+
+]);
+
+for (i = 0; i <= 20; i++) {
+    if (i % 2 == 0){
+        console.log(cat);
+    } else {
+        
+        console.log("Love me, pet me! HSSSSSS!");
+    }
+}
