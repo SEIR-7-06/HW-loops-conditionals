c3913d551809ea82b6fb763a30220e07a7d7dda4
xsatyax <satyasette1@gmail.com>
2021-07-12 10:56:34 -0700

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -42,3 +42,37 @@ function getRandomInt(max) {
           console.log("Love me, pet me! HSSSSSS")
       }
   }
+
+  // WORKING WITH ARRAYS
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. 
+// Remove "Kenny" from the `kenny` array and replace it with "Gameboy".
+
+kenny.splice(0,1,'Gameboy')
+
+// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older.  
+// Don't just hard code `187`--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+
+jimClark[1] = jimClark[1]+1
+
+// 3. Ryan is Batman maybe. Or possibly Robin. 
+// Change Ryan's hometown from "Denver" to "Gotham City".
+
+ryan[2] = "Gotham City"
+
+// 4. Reuben left Durham 5 years ago to come to Chicago. 
+// First, remove "Durham" from Reuben's array, and then add "Chicago".
+
+reuben.splice(2,1,'Chicago')
+
+// 5. Jim Haff could be literally anywhere in the world. 
+// Remove "All cities" from his array, then pick any 3 cities you like, 
+// and add them to Jim's array. If you did it in 3 lines of code that's fine, but 
+
+jimHaff.splice(2,1,'Vail', 'Jackson', 'Aspen')
